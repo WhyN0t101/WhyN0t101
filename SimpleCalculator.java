af840822a28f791cf488e063322f7636b725b384
WhyN0t101 <80111844+WhyN0t101@users.noreply.github.com>
2021-03-14 16:43:55 +0000

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,8 +1,8 @@
 
-package first;
+package SimpleCalculator;
 import java.util.Scanner;
 
-public class First {
+public class SimpleCalculator {
 
     public static void main(String[] args) {
         System.out.println("First Java Project Designed By Me\n");
